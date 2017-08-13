c694c89db23f1191b6b8e28fd22236711da6357d
Patrick Lewis <plewis@looisville.com>
2017-08-13 09:15:31 -0700

    add isEnabled to allow bypass of plugin

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 
 function WebpackTouch(options) {
-  var options = options || {}
+  var options = options || {};
   this.filename = options.filename;
   this.delay = options.delay? parseInt(options.delay) : 0;
   this.ignoreOnError = options.ignoreOnError;
+  this.isEnabled = options.isEnabled !== undefined ? options.isEnabled : true;
   if (!this.filename) {
     throw new Error('Require filename option')
   }
@@ -16,10 +17,13 @@ WebpackTouch.prototype.write = function() {
       console.error(err)
     }
   });
-}
+};
 
 WebpackTouch.prototype.apply = function(compiler) {
   compiler.plugin("done", function(stats) {
+    if (!this.isEnabled) {
+      return;
+    }
     if (this.ignoreOnError) {
       if (stats.hasErrors()) {
         console.log("Not touch on error");
@@ -27,7 +31,7 @@ WebpackTouch.prototype.apply = function(compiler) {
       }
     }
     if (this.delay) {
-      setTimeout(this.write.bind(this), this.delay)
+      setTimeout(this.write.bind(this), this.delay);
       return
     }
     this.write();
